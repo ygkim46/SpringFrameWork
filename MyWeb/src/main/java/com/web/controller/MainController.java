543eab09a3d8c8de24c601621f334fff2f8f283a
김영규 <youngkyu@lfpartners.co.kr>
2019-05-24 10:41:18 +0900

    1개 푸쉬

--- MyWeb/src/main/java/com/web/controller/MainController.java
+++ MyWeb/src/main/java/com/web/controller/MainController.java
@@ -41,7 +41,6 @@ public class MainController {
 	/* 머지됨 */
 	/* 머지인가 */
 	/* test1 */
-	/* test2 */
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	@Autowired
